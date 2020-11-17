4969833a894028c85c7d9f44b174a6b229696f48
kamils224 <kamil.sagalara@gmail.com>
2020-11-17 02:57:03 +0100

    Password reset, urls update

--- api_accounts/urls.py
+++ api_accounts/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
+from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.authtoken import views
-from api_accounts.views import UserCreateViewSet
+from api_accounts.views import UserCreateView, CustomAuthToken, ChangePasswordView
 
 urlpatterns = [
-    path('register/', UserCreateViewSet.as_view()),
-    path('authenticate/', views.obtain_auth_token),
+    path('register/', UserCreateView.as_view()),
+    path('authenticate/', CustomAuthToken.as_view()),
+    path('password_reset/', ChangePasswordView.as_view())
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- api_accounts/views.py
+++ api_accounts/views.py
@@ -2,14 +2,33 @@
 from rest_framework import generics, status
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
 from api_accounts.models import User
-from api_accounts.serializers import UserSerializer
+from api_accounts.serializers import UserSerializer, ChangePasswordSerializer
 
 
-class UserCreateViewSet(generics.CreateAPIView):
+class CustomAuthToken(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'username': user.username,
+            'email': user.email
+        })
+
+
+class UserCreateView(generics.CreateAPIView):
+    """
+    An endpoint for creating user.
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
@@ -23,5 +42,33 @@ class UserCreateViewSet(generics.CreateAPIView):
                 token, created = Token.objects.get_or_create(user=user)
                 json_result['token'] = token.key
                 return Response(json_result, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ChangePasswordView(generics.UpdateAPIView):
+    """
+    An endpoint for changing password.
+    """
+    serializer_class = ChangePasswordSerializer
+    model = User
+    permission_classes = [IsAuthenticated]
+
+    def update(self, request, *args, **kwargs):
+        user = self.request.user
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            if not user.check_password(serializer.data.get("old_password")):
+                return Response({'old_password': 'Wrong password.'}, status=status.HTTP_400_BAD_REQUEST)
+            user.set_password(serializer.data.get('new_password'))
+            user.save()
+            response = {
+                'status': 'success',
+                'code': status.HTTP_200_OK,
+                'message': 'Password updated successfully',
+                'data': []
+            }
+            return Response(response)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
