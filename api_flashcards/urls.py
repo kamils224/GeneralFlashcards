88efc74ed32e32f7a5235d6dcbaa396efb02d9da
kamils224 <kamil.sagalara@gmail.com>
2020-11-21 00:16:23 +0100

    Urls basenames added

--- api_flashcards/urls.py
+++ api_flashcards/urls.py
@@ -3,9 +3,11 @@ from rest_framework.routers import DefaultRouter
 
 from api_flashcards import views
 
+app_name = 'api_flashcards'
+
 router = DefaultRouter()
-router.register('collection', views.FlashcardsCollectionViewSet)
-router.register('flashcard', views.FlashcardViewSet)
+router.register('collection', views.FlashcardsCollectionViewSet, basename='collection')
+router.register('flashcard', views.FlashcardViewSet, basename='flashcard')
 
 urlpatterns = [
     path('', include(router.urls))
